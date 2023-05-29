063ba648ac0d8ed7caba6317524c7d43d570a7d0
Dmitriy <dmitriy.shchebetovskyi@gmail.com>
2023-05-29 20:06:57 +0300

    Initial commit

---
+++ lesson_1.py
@@ -0,0 +1,35 @@
+# 1
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+#
+# print(num1 + num2 + num3)
+# print(num1 * num2 * num3)
+
+# 2
+# S = (AC · BD) / 2.
+
+# first_d = int(input("Enter first diagonal: "))
+# second_d = int(input("Enter second diagonal: "))
+# area = (first_d * second_d) / 2
+# print(area)
+
+# 3
+# number = int(input("Enter 4-digit number: "))
+# print(number)
+# # 4321
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number % 100 // 10
+# n4 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+
+################################################################
+# Ctrl + C - copy
+
+
